1a4dc1983150a17a93f924bf028bd1d66100b2da
Yash Dixit <144922500+Yash10730@users.noreply.github.com>
2025-06-15 15:40:49 +0530

    Time: 4 ms (49.91%) | Memory: 53.3 MB (38.99%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int majorityCount = nums.length / 2;
-        HashMap<Integer, Integer> mp = new HashMap<>();
-
-        for (int num : nums) {
-            mp.put(num, mp.getOrDefault(num, 0) + 1);
-
-            if (mp.get(num) > majorityCount) {
-                return num;
-            }
-        }
-
-        // According to the problem, a majority element always exists
-        return -1; // This line will never be reached if input is valid
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
