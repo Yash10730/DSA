b5d64cab3223d7f8753feea6a498899e537f6d18
Yash Dixit <144922500+Yash10730@users.noreply.github.com>
2025-06-23 15:00:13 +0530

    Time: 1 ms (3.84%) | Memory: 42.6 MB (11.77%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,10 +1,14 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return i;
-            }
+    public int search(int[] nums, int target){
+        int n = nums.length, temp = n;
+        for(int i = 0; i < n-1; i++){
+            if(nums[i] > nums[i+1])
+            temp = i+1;
         }
+        int x = Arrays.binarySearch(Arrays.copyOfRange(nums, 0, temp), target);
+        int y = Arrays.binarySearch(Arrays.copyOfRange(nums, temp, n), target);
+        if(y >= 0) return y + temp;
+        if(x >= 0) return x;
         return -1;
     }
 }
