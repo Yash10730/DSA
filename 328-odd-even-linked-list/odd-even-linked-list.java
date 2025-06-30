9601edf6a4d82a583a7b36814fb2e1da823ad5c7
Yash Dixit <144922500+Yash10730@users.noreply.github.com>
2025-06-30 14:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (9.24%) - LeetSync

---
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even; // to attach at end of odd list
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead; // attach even list after odd list
+        return head;
+    }
+}
