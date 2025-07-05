deefd30e5a495dd15c0058a73ea74e5fac8edfd9
Yash Dixit <144922500+Yash10730@users.noreply.github.com>
2025-07-05 16:40:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Implement stack using array/implement-stack-using-array.java
@@ -0,0 +1,21 @@
+class MyStack {
+    private int[] arr;
+    private int top;
+Stack<Integer> st=new Stack<>();
+    public MyStack() {
+        arr = new int[1000];
+        top = -1;
+    
+    }
+
+    public void push(int x) {
+        // Your Code
+        arr[++top]=x;
+    }
+
+    public int pop() {
+        // Your Code
+          if(top < 0) return -1;
+        return arr[top--];
+    }
+}
