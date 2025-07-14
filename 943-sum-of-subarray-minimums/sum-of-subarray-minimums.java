bdf30fbf803f3971a2efe0b3f492d1c23f915cad
Yash Dixit <144922500+Yash10730@users.noreply.github.com>
2025-07-14 22:49:40 +0530

    Time: 72 ms (38.23%) | Memory: 51.4 MB (46.98%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,25 +1,55 @@
-class Solution {
-    public int sumSubarrayMins(int[] arr) {
-        Stack<int[]> stack = new Stack<>();
-        int[] res = new int[arr.length];
-        
-        for (int i = 0; i < arr.length; i++) {
-            // use ">=" to deal with duplicate elements
-            while (!stack.isEmpty() && stack.peek()[0] >= arr[i]) {
-                stack.pop();
+public class Solution {
+    public int[] getNSL(int[] arr, int n) {
+        int[] result = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                st.pop();
             }
 
-            int j = stack.isEmpty() ? -1 : stack.peek()[1];
-            res[i] = stack.isEmpty() ? arr[i] * (i + 1) : res[j] + arr[i] * (i - j);
-            stack.push(new int[]{arr[i], i});
+            result[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
         }
 
-        long MOD = (long) 1000000007;
-        long ans = 0;
+        return result;
+    }
 
-        for (int i = 0; i < arr.length; i++) {
-            ans = (ans + (long) res[i]) % MOD;
+    public int[] getNSR(int[] arr, int n) {
+        int[] result = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+
+            result[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
         }
-        return (int) ans;        
+
+        return result;
+    }
+
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+
+        int[] NSL = getNSL(arr, n);
+        int[] NSR = getNSR(arr, n);
+
+        long sum = 0;
+        int M = 1000000007;
+
+        for (int i = 0; i < n; i++) {
+            long d1 = i - NSL[i];
+            long d2 = NSR[i] - i;
+            
+            long totalWaysForIMin = d1 * d2;
+            long sumIInTotalWays = arr[i] * totalWaysForIMin;
+
+            sum = (sum + sumIInTotalWays) % M;
+        }
+
+        return (int) sum;
     }
 }
