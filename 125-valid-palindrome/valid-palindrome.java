5eded0915b2c2f33fc502d6de8367b7fabce146c
Yash Dixit <144922500+Yash10730@users.noreply.github.com>
2025-07-23 00:18:17 +0530

    Time: 20 ms (11.25%) | Memory: 45 MB (29.80%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,20 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // Step 1: Remove non-alphanumeric characters and convert to lowercase
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        // Step 2: Check if it reads the same forward and backward
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
+         String ans = s.replaceAll("\\s+", "").toLowerCase();
+             String result = ans.replaceAll("[^a-zA-Z0-9]", "");
+         if(s==" ")return true;
+         char ch[]=result.toCharArray();
+         int n=ch.length;
+      
+         int i=0;
+         int j=n-1;
+         System.out.print(ans);
+     while(i<=j){
+        if(ch[i]!=ch[j]){
+            return false;
+        }else{
+            i++;
+            j--;
+            
         }
+     }
+       
 
-        return true;
+    
+    return true;
     }
 }
