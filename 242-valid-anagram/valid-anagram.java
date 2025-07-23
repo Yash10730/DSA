ff300b678e605034a5887bb54919523e87fbe080
Yash Dixit <144922500+Yash10730@users.noreply.github.com>
2025-07-24 00:00:36 +0530

    Time: 20 ms (11.25%) | Memory: 45 MB (10.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,10 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        char[] a = s.toCharArray();
-        char[] b = t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a, b);
-    }
+        if(s.length()!=t.length())return false;
+        HashMap<Character,Integer> m1=new HashMap<>();
+        HashMap<Character,Integer> m2=new HashMap<>();
+        char ch[]=s.toCharArray();
+        char ch1[]=t.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            m1.put(ch[i], m1.getOrDefault(ch[i], 0) + 1);
+            m2.put(ch1[i], m2.getOrDefault(ch1[i], 0) + 1);
+        }
+         return m1.equals(m2);
+}
 }
