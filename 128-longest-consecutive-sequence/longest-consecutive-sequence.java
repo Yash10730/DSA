e92b10f4cd5be72266074a691364f7e8f23c37be
Yash Dixit <144922500+Yash10730@users.noreply.github.com>
2025-08-10 22:30:20 +0530

    Time: 29 ms (76.59%) | Memory: 67.8 MB (35.10%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,20 +1,24 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Set<Integer> ordered = new TreeSet<>();
-        for (int n : nums) ordered.add(n);
-
-        int longest = 0, curr = 0;
-        Integer prev = null;
-        for (int n : ordered) {
-            if (prev == null || n == prev + 1) {
-                curr++;
-            } else {
-                curr = 1;
-            }
-            prev = n;
-            longest = Math.max(longest, curr);
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
         }
 
-        return longest;
+        int longest = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int length = 1;
+
+                while (numSet.contains(num + length)) {
+                    length++;
+                }
+
+                longest = Math.max(longest, length);
+            }
+        }
+
+        return longest;        
     }
 }
